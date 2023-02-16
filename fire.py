282385c13a0fd780296ed077551c690dd04d3915
Daimzz446 <Pilott21st@gmail.com>
2023-02-16 21:37:18 +0300

    finished parser

--- fire.py
+++ fire.py
@@ -1,5 +1,12 @@
 All={}
 
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("live-node-firebase-adminsdk-53jk9-f26391514d.json")
+firebase_admin.initialize_app(cred)
+
+
 def save(m):
     with open("Manager.m","wb") as f:
         pickle.dump(m,f)
